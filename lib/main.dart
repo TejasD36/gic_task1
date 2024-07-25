6feb03f33c9f7b6ac7e6aa583a2e6c328e2c1495
Tejas Dudhal <116336036+TejasD36@users.noreply.github.com>
2024-07-25 12:58:37 +0530

    Add initial code and update .gitignore to exclude gradle directories

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,13 @@
+import 'package:customer_product_management_system/screens/customer_panel/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() {
 
   WidgetsFlutterBinding.ensureInitialized();
+    Firebase.initializeApp();
 
-  if(kIsWeb){
-    await Firebase.initializeApp(options: const FirebaseOptions(
-        apiKey: "AIzaSyALhC9hmQuzzRXSziyXZbjOl_wmSPNOsD0",
-        authDomain: "cutomerprod.firebaseapp.com",
-        projectId: "cutomerprod",
-        storageBucket: "cutomerprod.appspot.com",
-        messagingSenderId: "587248508057",
-        appId: "1:587248508057:web:9fc7df0b29b9e6e7b92c08",
-        measurementId: "G-HK5BBXKLJD"
-    ));
-  }
-  else{
-    await Firebase.initializeApp();
-  }
 
   runApp(const MyApp());
 }
@@ -36,60 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MainScreen(),
     );
   }
 }
